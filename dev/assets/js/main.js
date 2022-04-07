71cfe2939037e728d8be0709e1d34a27ec2bb45f
AGL_Lê Lạc Bình (ビン) <le_lac_binh@allgrow-labo.jp>
2022-04-07 10:35:34 +0700

    Finish TOP, Update main.js

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     //Add padding top
-    $('main').css('padding-top',$('.c-header').height())
+    $('main').css({
+        'padding-top' : $('.c-header').outerHeight(),
+        'padding-bottom' : $('.c-footer').outerHeight()
+    })
+
     // Header icon click
     $(".menu-trigger").click(function() {
         $(this).toggleClass('active');
