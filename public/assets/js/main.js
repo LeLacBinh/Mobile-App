5b24f258d010f4c7461a63c11e1e62a279da8c36
AGL_Lê Lạc Bình (ビン) <le_lac_binh@allgrow-labo.jp>
2022-04-07 08:10:37 +0700

    before pull from master

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     // Header icon click
     $(".menu-trigger").click(function() {
         $(this).toggleClass('active');
+        $('.c-header__menu').toggleClass('is-active');
     })
     $('.c-loading').fadeOut(1000)
 })
