5fe9462c9673aa9af0963d991573b518ea22798c
AGL_Nguyễn Cao Quốc Hùng(フン) <nguyen_cao_quoc_hung@allgrow-labo.jp>
2022-04-07 10:54:41 +0700

    public change

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
     //Add padding top
     $('main').css({
-        'padding-top' : $('.c-header').outerHeight(),
-        'padding-bottom' : $('.c-footer').outerHeight()
+        'padding-top': $('.c-header').outerHeight(),
+        'padding-bottom': $('.c-footer').outerHeight()
     })
 
     // Header icon click
     $(".menu-trigger").click(function() {
-<<<<<<< HEAD
-        $(this).toggleClass('active');
-        $('.c-header__menu').toggleClass('is-active');
-    })
-=======
             $(this).toggleClass('active');
             $('.c-header__menu').toggleClass('is-active');
 
@@ -19,6 +14,5 @@ $(document).ready(function() {
         //Add padding top
     $('main').css('padding-top', $('.c-header').outerHeight())
     $('main').css('padding-top', $('.c-headerv2').outerHeight())
->>>>>>> origin/hung_master
     $('.c-loading').fadeOut(1000);
 })
