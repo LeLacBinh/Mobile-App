010d1e377fdef50caf1333541feb68e79752fb69
AGL_Nguyễn Cao Quốc Hùng(フン) <nguyen_cao_quoc_hung@allgrow-labo.jp>
2022-04-07 08:26:42 +0700

    create : lucky-opinion

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     // Header icon click
     $(".menu-trigger").click(function() {
         $(this).toggleClass('active');
+        $('.c-header__menu').toggleClass('is-active');
+
     })
     $('.c-loading').fadeOut(1000)
 })
