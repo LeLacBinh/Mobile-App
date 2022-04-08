10c894771a9530be78adf2d3ad96df35b9fe1ee6
AGL_Nguyễn Cao Quốc Hùng(フン) <nguyen_cao_quoc_hung@allgrow-labo.jp>
2022-04-08 13:49:30 +0700

    create: like.html like-detail.html

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,13 +14,7 @@ $(document).ready(function() {
         //Add padding top
     $('main').css('padding-top', $('.c-header').outerHeight())
     $('main').css('padding-top', $('.c-headerv2').outerHeight())
-<<<<<<< HEAD
     setTimeout(function() {
         $('.c-loading').fadeOut(1000);
     }, 1000)
-=======
-    setTimeout(function(){
-        $('.c-loading').fadeOut(1000);
-    },1000)
->>>>>>> master
 })
