c00da2f3792185643bb01200d0b9d5e056c9e765
AGL_Nguyễn Cao Quốc Hùng(フン) <nguyen_cao_quoc_hung@allgrow-labo.jp>
2022-04-12 17:34:25 +0700

    create : challenge.html

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,40 +1,15 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-    var xValues = ["ホール", "キッチン", "マネジメント"];
-    var yValues = [89, 52, 70];
-    var barColors = ["#60a48d", "#8da9db", "#ffd44b"];
-
-    new Chart("skill_chart_01", {
-        type: "bar",
-        data: {
-            labels: xValues,
-            datasets: [{
-                backgroundColor: barColors,
-                data: yValues
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-            plugins: {
-                legend: { display: false }
-            }
-        }
-    });
-    $('#skill_chart_01').height(200)
-        //Add padding top
-=======
     $(".js-clickScroll").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
 
             $('html, body').animate({
-            scrollTop: $(hash).offset().top - $('.c-headerv2').outerHeight()
+                scrollTop: $(hash).offset().top - $('.c-headerv2').outerHeight()
             }, 800);
         }
-      });
+    });
     //Add padding top
->>>>>>> 2c089b80aa5c409eec1524a1f803f465bf6903dd
     $('main').css({
         'padding-top': $('#the_header').outerHeight(),
         'padding-bottom': $('#the_footer').outerHeight()
