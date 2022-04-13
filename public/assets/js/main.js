5cc8386a7a230fe9b16c7f33681ef877493093a2
AGL_Lê Lạc Bình (ビン) <le_lac_binh@allgrow-labo.jp>
2022-04-13 11:19:38 +0700

    Finish daily-report.html

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     "padding-bottom": $("#the_footer").outerHeight(),
   });
 
-<<<<<<< HEAD
   // Header icon click
   $(".menu-trigger").click(function () {
     $(this).toggleClass("active");
@@ -20,6 +19,7 @@ $(document).ready(function () {
   }, 1000);
 });
 
+// --- Create a Simple Calendar ---
 function js_calendar() {
   var currentDate = new Date();
   function generateCalendar(d) {
@@ -57,6 +57,7 @@ function js_calendar() {
     var start = new Date(d.getFullYear(), d.getMonth()).getDay();
     var cal = [];
     var day = 1;
+    let the_date,no_note;
     for (var i = 0; i <= 6; i++) {
       cal.push(["<tr>"]);
       for (var j = 0; j < 7; j++) {
@@ -68,7 +69,17 @@ function js_calendar() {
           if (i === 1 && j < start) {
             cal[i].push("<td>&nbsp;</td>");
           } else {
-            cal[i].push('<td class="day"><span>' + day++ + "</span></td>");
+            the_date = currentDate.getFullYear()+'年'+details.months[d.getMonth()] + day+'日'
+            no_note = true
+            for( var _i = 0; _i < notice.length; _i++ ){
+              if(notice[_i].date === the_date){
+                no_note = false;
+                cal[i].push('<td onclick="calendar_show_content(this)" class="day" data-date="'+ the_date +'" data-note="'+ _i +'"><span>' + day++ + '</span><p>' + notice[_i].desc.earn + '</p></td>');
+              }
+            }
+            if(no_note){
+              cal[i].push('<td onclick="calendar_show_content(this)" class="day" data-date="'+ the_date +'" data-note="no"><span>' + day++ + '</span></td>');
+            }
           }
         }
       }
@@ -90,6 +101,7 @@ function js_calendar() {
         $(this).removeClass("hover");
       });
   }
+  // Function Change Month when click
   $("#left").click(function () {
     $(".js-table").text("");
     if (currentDate.getMonth() === 0) {
@@ -118,29 +130,31 @@ function js_calendar() {
   });
   generateCalendar(currentDate);
 }
-=======
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top - $('.c-headerv2').outerHeight()
-            }, 800);
-        }
-    });
-    //Add padding top
-    $('main').css({
-        'padding-top': $('#the_header').outerHeight(),
-        'padding-bottom': $('#the_footer').outerHeight()
-    })
 
-    // Header icon click
-    $(".menu-trigger").click(function() {
-            $(this).toggleClass('active');
-            $('.c-header__menu').toggleClass('is-active');
-
-        })
-        //Add padding top
-    $('main').css('padding-top', $('.c-header').outerHeight())
-    $('main').css('padding-top', $('.c-headerv2').outerHeight())
-    setTimeout(function() {
-        $('.c-loading').fadeOut(1000);
-    }, 1000)
-})
->>>>>>> master
+function calendar_show_content(_t){
+  // Show Content Box
+  if(!$('.p-daily__content').hasClass('p-daily__content--show')){
+    $('.p-daily__content').addClass('p-daily__content--show')
+  }
+  // Show detail information
+  const note_id = $(_t).data('note')
+  
+  if(note_id != 'no'){
+    const the_target = notice[note_id].desc
+    $('.p-daily__no-note').hide()
+    $('.p-daily__has-note').fadeIn(500)
+    // Replace the data
+    $('#date_earn').html(the_target.earn)
+    $('#date_pairs').html(the_target.pairs.lunch + the_target.pairs.dinner)
+    $('#date_pairs_lunch').html(the_target.pairs.lunch)
+    $('#date_pairs_dinner').html(the_target.pairs.dinner)
+    $('#date_people').html(the_target.people.lunch + the_target.people.dinner)
+    $('#date_people_lunch').html(the_target.people.lunch)
+    $('#date_people_dinner').html(the_target.people.dinner)
+  }else{
+    $('.p-daily__has-note').hide()
+    $('.p-daily__no-note').fadeIn(500)
+  }
+  // Change The title Content box
+  $('.p-daily__head h3').html($(_t).data('date'))
+}
